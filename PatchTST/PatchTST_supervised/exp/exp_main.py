245cb855d8a19c6755729b9439b7a95a1ee4424b
Simon Kral <simonkral99@icloud.com>
2025-05-28 12:32:02 +0200

    Adapt model name of DWSC

--- PatchTST/PatchTST_supervised/exp/exp_main.py
+++ PatchTST/PatchTST_supervised/exp/exp_main.py
@@ -1,6 +1,6 @@
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
-from models import Informer, Autoformer, Transformer, DLinear, Linear, NLinear, PatchTST, _Simon_Conv
+from models import Informer, Autoformer, Transformer, DLinear, Linear, NLinear, PatchTST, _Simon_DWSC
 from utils.tools import EarlyStopping, adjust_learning_rate, visual, test_params_flop
 from utils.metrics import metric
 
@@ -34,7 +34,7 @@ class Exp_Main(Exp_Basic):
             'NLinear': NLinear,
             'Linear': Linear,
             'PatchTST': PatchTST,
-            'Simon_Conv': _Simon_Conv,
+            'Simon_DWSC': _Simon_DWSC,
         }
         model = model_dict[self.args.model].Model(self.args).float()
 
