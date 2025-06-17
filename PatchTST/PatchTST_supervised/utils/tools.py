7e1a84cd883710c998c8d2fa4bf2fed9f48e826a
Simon Kral <simonkral99@icloud.com>
2025-06-17 13:57:33 +0200

    Added class UnfreezeParam

--- PatchTST/PatchTST_supervised/utils/tools.py
+++ PatchTST/PatchTST_supervised/utils/tools.py
@@ -68,6 +68,24 @@ class EarlyStopping:
         torch.save(model.state_dict(), path + '/' + 'checkpoint.pth')
         self.val_loss_min = val_loss
 
+class UnfreezeParam:
+    def __init__(self, patience=5):
+        self.patience = patience
+        self.counter = 0
+        self.best_score = None
+        self.unfreeze = False
+
+    def __call__(self, val_loss):
+        score = -val_loss
+        if self.best_score is None:
+            self.best_score = score
+        elif score < self.best_score:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.unfreeze = True
+        else:
+            self.best_score = score
+            self.counter = 0
 
 class dotdict(dict):
     """dot.notation access to dictionary attributes"""
